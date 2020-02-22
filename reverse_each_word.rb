d78a92ea30b585bc617d20256ca6e03300a01d22
Ryna Truong <rynatruong@outlook.com>
2020-02-22 20:05:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(str)
   reversed_str = []
   word = str.split
-  word.each do |word|
+  word.collect each do |word|
     reversed_str.push(word.reverse)
   end
   return reversed_str.join(" ")
