a72f152a1b002bb963751372000b6eabd7424fa2
Ryna Truong <rynatruong@outlook.com>
2020-02-22 20:11:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,11 +6,3 @@ def reverse_each_word(str)
   end
   return reversed_str.join(" ")
 end
-
-def reverse_each word(str)
-  new_str = []
-  words = new_str.split
-  words.collect do |word|
-    new_str.push(word.reverse)
-  end 
-  return new_str.join(" ")
